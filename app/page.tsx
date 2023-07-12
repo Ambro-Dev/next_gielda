62de53219144b8b0cd2e07b1b7fffbe213a393b1
Ambro-Dev <piotr.ambroziak@ambro.dev>
2023-07-12 12:54:23 +0200

    edit page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-between p-24">
+    <main className="flex min-h-screen items-center justify-center p-24">
       <h1 className="text-4xl font-bold text-center">Next.js + Tailwind CSS</h1>
     </main>
   );
