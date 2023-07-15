8b31909d750dc9e6771af07e8f77ebd61c68d34b
Ambro-Dev <piotr.ambroziak@ambro.dev>
2023-07-15 12:21:28 +0200

    register

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,7 +1,17 @@
 import dbConnect from '@/lib/dbConnect'
 import User from '@/models/UserModel';
 import { NextRequest, NextResponse } from 'next/server'
+import { hashSync, genSaltSync } from 'bcrypt-ts';
 
+function validateEmail(email: string) {
+  const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  return re.test(email);
+}
+
+const hashPassword = (password: string) => {
+    const salt = genSaltSync(10);
+    return hashSync(password, salt);
+  }
 
 export async function GET(req: NextRequest) {
   await dbConnect();
@@ -10,12 +20,20 @@ export async function GET(req: NextRequest) {
 
 export async function POST(req: NextRequest) {
   await dbConnect();
+  
+  const {name, email, password} = await req.json();
 
-  const body = await req.json();
+  if (!name || !email || !password) {
+    return NextResponse.json({error: 'Please fill all fields'})
+  }
 
-  const user = await User.create(body);
+  if (!validateEmail(email)) {
+    return NextResponse.json({error: 'Please enter a valid email'})
+  }
 
-  console.log(user);
+  const hashedPassword = hashPassword(password);
+
+  const user = await User.create({name, email, password: hashedPassword});
 
   await user.save();
   return NextResponse.json({hello: 'User added!'})
