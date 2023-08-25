25b829dd7ed7914f5a3fdf84d853536f5f178e31
Ambro-Dev <piotr.ambroziak@ambro.dev>
2023-08-25 08:55:16 +0200

    update

--- components/SearchNearby.tsx
+++ components/SearchNearby.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from "react";
+"use client";
+
+import React, { use, useEffect, useState } from "react";
 import { Input } from "./ui/input";
 import {
   Tooltip,
@@ -9,19 +11,90 @@ import {
 import { Button } from "./ui/button";
 import TransportMapSelector from "./TransportMapSelector";
 import { LatLng } from "use-places-autocomplete";
+import { useRouter, useSearchParams } from "next/navigation";
 
 type Props = {};
 
 const SearchNearby = (props: Props) => {
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+
+  const [searchString, setSearchString] = useState<string>(
+    searchParams.toString()
+  );
+
   const [from, setFrom] = useState<LatLng | null>(null);
   const [to, setTo] = useState<LatLng | null>(null);
+
+  const [fromString, setFromString] = useState<string | null>(
+    searchParams.get("from")
+  );
+  const [toString, setToString] = useState<string | null>(
+    searchParams.get("to")
+  );
+
+  useEffect(() => {
+    setFromString(searchParams.get("from"));
+    setToString(searchParams.get("to"));
+    console.log(searchParams.toString());
+  }, [searchParams]);
+
+  useEffect(() => {
+    if (fromString) {
+      setFrom({
+        lat: parseFloat(fromString.split(",")[0]),
+        lng: parseFloat(fromString.split(",")[1]),
+      });
+    }
+  }, [fromString]);
+
+  useEffect(() => {
+    if (toString) {
+      setTo({
+        lat: parseFloat(toString.split(",")[0]),
+        lng: parseFloat(toString.split(",")[1]),
+      });
+    }
+  }, [toString]);
+
+  const handleFromChange = (from: LatLng) => {
+    setFrom(from);
+    console.log(fromString);
+    console.log(`${from.lat},${from.lng}`);
+    console.log(fromString === `${from.lat},${from.lng}`);
+    if (fromString === `${from.lat},${from.lng}`) return;
+    else
+      setSearchString(
+        searchString.replace(
+          `from=${fromString}`,
+          `from=${from.lat},${from.lng}`
+        )
+      );
+  };
+
+  const handleToChange = (to: LatLng) => {
+    setTo(to);
+    if (toString === `${to.lat},${to.lng}`) return;
+    else
+      setSearchString(
+        searchString.replace(`to=${toString}`, `to=${to.lat},${to.lng}`)
+      );
+  };
+
+  const handleSearch = () => {
+    router.push(`/transport?${searchString}`);
+  };
 
   return (
     <div className="grid lg:grid-cols-2 grid-cols-1 w-full gap-4 py-7 px-3">
       <div className="w-full flex flex-col gap-4">
         <div className="w-full flex flex-row gap-4">
-          <TransportMapSelector setPlace={setFrom} placeholder="Skąd" />
-          <TransportMapSelector setPlace={setTo} placeholder="Dokąd" />
+          <TransportMapSelector
+            setPlace={handleFromChange}
+            placeholder="Skąd"
+          />
+          <TransportMapSelector setPlace={handleToChange} placeholder="Dokąd" />
         </div>
       </div>
 
@@ -29,7 +102,10 @@ const SearchNearby = (props: Props) => {
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger asChild>
-              <Button className="w-full hover:bg-neutral-800 bg-transparent border-2 hover:text-white border-neutral-800 text-black">
+              <Button
+                className="w-full hover:bg-neutral-800 bg-transparent border-2 hover:text-white border-neutral-800 text-black"
+                onClick={handleSearch}
+              >
                 Szukaj przy trasie
               </Button>
             </TooltipTrigger>
