849304c9be36cff1d1b7cde028b41f32a6fe8956
Ambro-Dev <piotr.ambroziak@ambro.dev>
2023-09-24 23:12:53 +0200

    layout change

--- app/(public)/(auth)/reset-password/layout.tsx
+++ app/(public)/(auth)/reset-password/layout.tsx
@@ -1,5 +1,8 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import type { Metadata } from "next";
+import { getServerSession } from "next-auth";
 import { Inter } from "next/font/google";
+import { redirect } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,5 +16,9 @@ export default async function LoginLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
+  if (session) redirect("/");
+
   return <>{children}</>;
 }
