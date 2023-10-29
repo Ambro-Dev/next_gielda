6f665f2cfe51fadd39bdea451b2f152514871ae0
Ambro-Dev <piotr.ambroziak@ambro.dev>
2023-10-29 02:49:20 +0200

    offers deletion update

---
+++ app/api/offers/delete/route.ts
@@ -0,0 +1,152 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prismadb";
+
+export async function PUT(req: NextRequest) {
+  const body = await req.json();
+
+  const { adminId, schoolId } = body;
+
+  if (!adminId) {
+    return NextResponse.json({ error: "Brakuje ID admina", status: 400 });
+  }
+
+  const isAdmin = await prisma.user.findUnique({
+    where: {
+      id: adminId,
+    },
+    select: {
+      role: true,
+    },
+  });
+
+  if (!isAdmin) {
+    return NextResponse.json({
+      error: "Nie znaleziono użytkownika",
+      status: 404,
+    });
+  }
+
+  if (isAdmin.role === "admin" || isAdmin.role === "user") {
+    if (!schoolId) {
+      return NextResponse.json({ error: "Brakuje ID szkoły", status: 400 });
+    }
+
+    const changingSchool = await prisma.school.findUnique({
+      where: {
+        id: schoolId,
+      },
+    });
+
+    if (!changingSchool) {
+      return NextResponse.json({
+        error: "Nie znaleziono szkoły",
+        status: 404,
+      });
+    }
+
+    const schoolOffers = await prisma.offer.findMany({
+      where: {
+        transport: {
+          school: {
+            id: schoolId,
+          },
+        },
+      },
+    });
+
+    if (schoolOffers.length === 0) {
+      return NextResponse.json({
+        error: "Szkoła nie ma żadnych ofert",
+        status: 404,
+      });
+    }
+
+    await prisma.offer.deleteMany({
+      where: {
+        transport: {
+          school: {
+            id: schoolId,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json({
+      message:
+        "Proces usuwania przebiegł pomyślnie, oferty dla szkoły usuniętę",
+      status: 200,
+    });
+  }
+
+  const admin = await prisma.user.findUnique({
+    where: {
+      id: adminId,
+    },
+    select: {
+      school: {
+        select: {
+          id: true,
+        },
+      },
+    },
+  });
+
+  if (!admin) {
+    return NextResponse.json({
+      error: "Nie znaleziono admina",
+      status: 404,
+    });
+  }
+
+  if (!admin.school) {
+    return NextResponse.json({
+      error: "Użytkownik nie jest adminem żadnej szkoły",
+      status: 404,
+    });
+  }
+
+  const school = await prisma.school.findUnique({
+    where: {
+      id: admin.school.id,
+    },
+  });
+
+  if (!school) {
+    return NextResponse.json({
+      error: "Nie znaleziono szkoły",
+      status: 404,
+    });
+  }
+
+  const schoolOffers = await prisma.offer.findMany({
+    where: {
+      transport: {
+        school: {
+          id: school.id,
+        },
+      },
+    },
+  });
+
+  if (schoolOffers.length === 0) {
+    return NextResponse.json({
+      error: "Szkoła nie ma żadnych ofert",
+      status: 404,
+    });
+  }
+
+  await prisma.offer.deleteMany({
+    where: {
+      transport: {
+        school: {
+          id: school.id,
+        },
+      },
+    },
+  });
+
+  return NextResponse.json({
+    message: "Proces usuwania przebiegł pomyślnie, oferty usuniętę",
+    status: 200,
+  });
+}
