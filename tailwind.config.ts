9b7a91a5de7caecb047aa751d76433770f05b7f1
Ambro-Dev <piotr.ambroziak@ambro.dev>
2023-12-26 00:48:44 +0100

    sheet updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss"
 
-import { withUt } from "uploadthing/tw";
-
-module.exports = withUt({
+const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
     container: {
       center: true,
@@ -61,12 +60,12 @@ module.exports = withUt({
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0" },
         },
       },
       animation: {
@@ -76,5 +75,6 @@ module.exports = withUt({
     },
   },
   plugins: [require("tailwindcss-animate")],
-  plugins: [require("daisyui")],
-});
+} satisfies Config
+
+export default config
